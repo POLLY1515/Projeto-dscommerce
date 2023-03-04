0bdafb92e52ac2aa3e548aae8b43a2baf710df7f
POLLY1515 <amarantepolly2030@gmail.com>
2023-03-04 01:15:25 -0300

    Order

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +27,8 @@ public class User {
 	private String password;
 	
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		// TODO Auto-generated constructor stub
@@ -86,6 +91,13 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	
 
 	@Override
 	public int hashCode() {
