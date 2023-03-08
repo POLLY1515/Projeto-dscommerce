6f3cb8128fb40f2fca0f8dfa17c46caf683029f6
POLLY1515 <amarantepolly2030@gmail.com>
2023-03-07 21:08:03 -0300

    Muitos para muitos com classe de associação parte 1

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -38,6 +39,10 @@ public class Product {
 	    joinColumns =  @JoinColumn(name = "product_id"),
 	    inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 		// TODO Auto-generated constructor stub
